30bd920cfc418a1feb000ca5c4b5a281e4b69716
Harika Mulumudi <harikamulumudi@navyas-MacBook-Pro.local>
2019-11-28 18:43:47 -0500

    end time

--- twitter/lib/simulation.ex
+++ twitter/lib/simulation.ex
@@ -3,72 +3,7 @@ defmodule Twitter.Simulator do
 
   def simulate(numUsers, numTweets, clients, engine) do
 
-  users_table = :ets.new(:users_table, [:named_table,:public])
-  #users = :ets.new(:users, [:named_table,:public])
-
-
-  :ets.insert_new(users_table, {"user1", ["pwd_user1","user1@mail.com",1]})
-  :ets.insert_new(users_table, {"user2", ["pwd_user2","user2@mail.com",0]})
-  :ets.insert_new(users_table, {"user3", ["pwd_user3","user3@mail.com",0]})
-  :ets.insert_new(users_table, {"user4", ["pwd_user4","user4@mail.com",0]})
-  :ets.insert_new(users_table, {"user5", ["pwd_user5","user5@mail.com",0]})
-
-  tweets_table = :ets.new(:tweets_table, [:named_table,:public])
-
-  :ets.insert_new(tweets_table, {"T1", "#DOS project uses #GenServer for doing cast @user3"})
-  :ets.insert_new(tweets_table, {"T2", "#Elixir vs #Scala"})
-  :ets.insert_new(tweets_table, {"T3", "#Concurrency @user2 is main concept of #DOS"})
-  :ets.insert_new(tweets_table, {"T4", "@user2 department"})
-  :ets.insert_new(tweets_table, {"T5", "Project is due on @user1"})
-
-  #subscribers = bag.new()
-  #Bag.add_new!(subscribers, {})
-
-  subscribers_table = :ets.new(:subscribers_table, [:named_table,:public])
-
-  :ets.insert_new(subscribers_table, {"user1", ["user3","user2","user6"]})
-  :ets.insert_new(subscribers_table, {"user2", ["user6"]})
-  :ets.insert_new(subscribers_table, {"user3", ["user1","user5","user4"]})
-  :ets.insert_new(subscribers_table, {"user4", ["user5"]})
-  :ets.insert_new(subscribers_table, {"user5", ["user3","user2"]})
-
-  subscribedTo_table = :ets.new(:subscribedTo_table, [:named_table,:public])
-
-  :ets.insert_new(subscribedTo_table, {"user1", ["user5","user6"]})
-  :ets.insert_new(subscribedTo_table, {"user2", ["user1","user3"]})
-  :ets.insert_new(subscribedTo_table, {"user3", ["user1","user6"]})
-  :ets.insert_new(subscribedTo_table, {"user4", ["user2","user3"]})
-  :ets.insert_new(subscribedTo_table, {"user5", []})
-
-  tweetUserMap_table = :ets.new(:tweetUserMap_table, [:named_table,:public])
-
-  :ets.insert_new(tweetUserMap_table, {"user1", ["T1","T5"]})
-  :ets.insert_new(tweetUserMap_table, {"user2", ["T2","T6"]})
-  :ets.insert_new(tweetUserMap_table, {"user3", ["T8"]})
-  :ets.insert_new(tweetUserMap_table, {"user4", ["T4","T9"]})
-  :ets.insert_new(tweetUserMap_table, {"user5", ["T3"]})
-
-  mentionUserMap_table = :ets.new(:mentionUserMap_table, [:named_table,:public])
-
-  :ets.insert_new(mentionUserMap_table, {"user1", ["T5"]})
-  :ets.insert_new(mentionUserMap_table, {"user2", ["T3","T4"]})
-  :ets.insert_new(mentionUserMap_table, {"user3", ["T1"]})
-  :ets.insert_new(mentionUserMap_table, {"user4", []})
-  :ets.insert_new(mentionUserMap_table, {"user5", []})
-
-
-  hashTagTweetMap_table = :ets.new(:hashTagTweetMap_table, [:named_table,:public])
-
-  :ets.insert_new(hashTagTweetMap_table, {"#DOS", ["T1","T3"]})
-  :ets.insert_new(hashTagTweetMap_table, {"#GenServer", ["T1"]})
-  :ets.insert_new(hashTagTweetMap_table, {"#Concurrency", ["T3"]})
-  :ets.insert_new(hashTagTweetMap_table, {"#Elixir", ["T2"]})
-  :ets.insert_new(hashTagTweetMap_table, {"#Scala", ["T2"]})
-
-  tableSize_table = :ets.new(:tableSize_table, [:named_table,:public])
-
-  :ets.insert_new(tableSize_table, {"users", 5})
-  :ets.insert_new(tableSize_table, {"tweets", 5})
+  
 
   simulation = :ets.new(:simulation, [:named_table,:public])
   :ets.insert_new(simulation, {"tweetCount", 0})
@@ -125,16 +60,13 @@ IO.inspect clients, label: "clientsAfter"
      GenServer.call(userSubscribingTo,{:getSubscribersOf, userName}) 
  end)
  
- IO.inspect "query subscribed to"
-
- queryUser = Enum.random(clients)
- IO.inspect queryUser, label: "queryUser"
- GenServer.call(queryUser,{:querySubscribedTo})
-
+ 
  tweetList=[]
  tweetList = tweetList ++ ["Tweet1","Tweet2","Tweet3","Tweet4","Tweet5","Tweet6","Tweet7","Tweet8","Tweet9","Tweet10"]
 
  IO.inspect "tweet"
+
+ startTime = System.system_time(:millisecond)
 
  Enum.each(clients, fn(user) ->
  Enum.map((1..numTweets), fn(x) ->
@@ -153,6 +85,16 @@ IO.inspect clients, label: "clientsAfter"
 #GenServer.call(engine,{:getTweet, 1})
 end)
  end)
+
+
+ #:timer.sleep(10000);
+
+ userToRetweet = Enum.random(clients)
+ IO.inspect userToRetweet, label: "userToRetweet"
+ GenServer.call(userToRetweet,{:returnStateTweets})
+
+
+
 
 
  deleteUser = Enum.random(clients)
@@ -231,21 +173,24 @@ end)
 
   #IO.inspect "Before Register"
 
-    infinite(numUsers*numTweets*0.8)
+    infinite(numUsers*numTweets*0.8, startTime)
    end
 
-   def infinite(total_tweets) do
+   def infinite(total_tweets, startTime) do
    [{_, num_tweets}] = :ets.lookup(:simulation, "tweetCount")
    #[{_, tr}] = :ets.lookup(:table, "tr")
    # IO.inspect {tr, count}
     if num_tweets >= total_tweets do
       #simulation = :ets.new(:simulation, [:named_table,:public])
       #:ets.insert_new(simulation, {"tweetCount", 0})
-      
+      currentTime = System.system_time(:millisecond)
+      endTime = currentTime - startTime
+      IO.puts "Convergence Achieved in = "<> Integer.to_string(endTime)<> "ms"
+      IO.puts("Converged")
       #IO.puts(maxHops)
     else
     IO.inspect num_tweets, label: "num_tweets"
-    infinite(total_tweets)
+    infinite(total_tweets, startTime)
    end
    end
 end
